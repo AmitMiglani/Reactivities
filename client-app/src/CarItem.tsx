e9b38cafbece8019913c8c4c37775abbb1892fe5
Amit Miglani <amitmiglani@gmail.com>
2019-10-19 07:43:14 +0000

    End of Section 3

--- client-app/src/CarItem.tsx
+++ client-app/src/CarItem.tsx
@@ -5,12 +5,12 @@ interface IProps {
     car: ICar
 }
 
-const CarItem: React.FC<IProps> = ({car}}) => {
+const CarItem: React.FC<IProps> = ({car}) => {
     return (
         <div>
             <h1>{car.color}</h1>
         </div>
     )
-}
+};
 
 export default CarItem
